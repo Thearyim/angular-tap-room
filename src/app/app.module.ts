fdf3c205e843892e694390e55bdf88dccf2bc9f2
ThearyIm <thearyim@gmail.com>
2019-04-06 17:34:03 -0700

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-// import { CompletenessPipe } from './completeness.pipe';
 import { KegListComponent } from './keg-list/keg-list.component';
 import { EditKegComponent } from './edit-keg/edit-keg.component';
 import { NewKegComponent } from './new-keg/new-keg.component';
